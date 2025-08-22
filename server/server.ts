2e130c69823e516038561c42b34b1d3ee2996e19
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-22 23:56:59 +0530

    feat(server): add server setup and database connection

---
+++ server/server.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './src/app';
+import config from  './src/config';
+
+
+dotenv.config();
+
+
+(async ()=> {
+    try {
+       await mongoose.connect(config.DATABASE_URL)
+       console.log("DATABASE CONNECTED SUCCESSFULLY")
+
+       app.on('error', (error) => {
+              console.error("ERROR :",error)
+              throw error;
+       })
+       
+       app.listen(config.PORT, () => {
+           console.log(`Server is Listening on http://localhost:${config.PORT}`)
+       })
+
+    } catch (error) {
+        console.error("ERROR :",error)
+        throw error;
+    }
+})()
