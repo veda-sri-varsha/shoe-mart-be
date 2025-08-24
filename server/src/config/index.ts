8fe0ec0bc37d5daed75e884472ebe62dfeeccb3d
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-24 23:23:31 +0530

    chore: add configuration variables

---
+++ server/src/config/index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+ PORT: process.env.PORT,
+  DATABASE_URL: process.env.DATABASE_URL as string,
+  JWT_SECRET: process.env.JWT_ACCESS_SECRET as string,
+  JWT_EXPIRATION: Number(process.env.JWT_ACCESS_EXPIRATION),
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+  // JWT_ACCESS_EXPIRATION: Number(process.env.JWT_ACCESS_EXPIRATION),
+  JWT_REFRESH_EXPIRATION: Number(process.env.JWT_REFRESH_EXPIRATION),
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+  RAZORPAY_KEY: process.env.RAZORPAY_KEY_ID as string,
+  RAZORPAY_SECRET: process.env.RAZORPAY_KEY_SECRET as string,
+  SMTP_HOST: process.env.SMTP_HOST as string,
+  SMTP_PORT: process.env.SMTP_PORT as string,
+  SMTP_USERNAME: process.env.SMTP_USERNAME as string,
+  SMTP_PASSWORD: process.env.SMTP_PASSWORD as string,
+};
+
+export default config;
