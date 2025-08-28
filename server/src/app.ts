5703a4eff2a32d7761ea8b466b097f9a57972f42
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-29 02:40:08 +0530

    feat(app): initialize express app with root, health check, and routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,46 +1,24 @@
 import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
-import User from "./models/user.schema";
-import UserAuth from "./middlewares/UserAuth";
 import router from "./routes/index";
-import cors from "cors"; 
 
-
-const app: express.Application = express();
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin: "http://localhost:3000", 
-  credentials: true,               
-}));
-
-
-app.get('/',(req,res) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send("Welcome to the E-Commerce API");
 });
 
+app.get("/healthcheck", (_req: Request, res: Response) => {
+  return res.status(200).json({
+    status: 200,
+    message: "OK",
+    data: "Health check passed",
+  });
+});
+
 app.use("/api", router);
-
-
-app.post("/sendRequest", UserAuth, async (req: Request, res: Response) => {
-  try {
-   const user = (req as any).user; 
-    console.log("Sending Connection Request");
-    res.send("Connection sent: " + user?.name);
-  } catch (err: any) {
-    res.status(500).send("Error sending request: " + err.message);
-  }
-});
-
-app.get("/feed", async (_req: Request, res: Response) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err: any) {
-    res.status(500).send("Error fetching feed: " + err.message);
-  }
-});
 
 export default app;
