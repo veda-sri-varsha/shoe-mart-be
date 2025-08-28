10944e4ee4a05dbea72839761fe090383bcb2fda
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-29 02:28:57 +0530

    feat(server): setup server and database connection

--- server/server.ts
+++ server/server.ts
@@ -1,30 +1,25 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import app from './src/app';
-import config from  './src/config/index';
-import { setupSwagger } from "../server/src/swagger";
-import express from 'express';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "./src/app";
+import config from "./src/config/index";
 
 dotenv.config();
 
-setupSwagger(app as express.Express);
+(async () => {
+  try {
+    await mongoose.connect(config.DATABASE_URL);
+    console.log("DATABASE CONNECTED SUCCESSFULLY");
 
-(async ()=> {
-    try {
-       await mongoose.connect(config.DATABASE_URL)
-       console.log("DATABASE CONNECTED SUCCESSFULLY")
+    app.on("error", (error) => {
+      console.error("ERROR :", error);
+      throw error;
+    });
 
-       app.on('error', (error) => {
-              console.error("ERROR :",error)
-              throw error;
-       })
-       
-       app.listen(config.PORT, () => {
-           console.log(`Server is Listening on http://localhost:${config.PORT}`)
-       })
-
-    } catch (error) {
-        console.error("ERROR :",error)
-        throw error;
-    }
-})()
+    app.listen(config.PORT, () => {
+      console.log(`Server is Listening on http://localhost:${config.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR :", error);
+    throw error;
+  }
+})();
