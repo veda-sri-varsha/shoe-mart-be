cae6f3aa8b2079712516210998a9fe856444e18f
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-29 15:08:19 +0530

    feat(routes): add authenticated and authorized product management routes

---
+++ server/src/routes/product.routes.ts
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import {
+  getAllProducts,
+  addProduct,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+  productByCollectionId,
+} from "../controllers/product.controller";
+import UserAuth, { authorize } from "../middlewares/UserAuth";
+import AuthRoles from "../constants/authRoles";
+
+const router: Router = Router();
+
+router.post("/", UserAuth, authorize(AuthRoles.ADMIN, AuthRoles.VENDOR), addProduct);
+router.put(
+  "/:id",
+  UserAuth,
+  authorize(AuthRoles.ADMIN, AuthRoles.VENDOR),
+  updateProduct
+);
+router.patch(
+  "/:id",
+  UserAuth,
+  authorize(AuthRoles.ADMIN, AuthRoles.VENDOR),
+  deleteProduct
+);
+router.get(
+  "/",
+  UserAuth,
+  getAllProducts
+);
+router.get(
+  "/:id",
+  UserAuth,
+  authorize(AuthRoles.ADMIN, AuthRoles.VENDOR),
+  getProductById
+);
+router.get(
+  "/collection/:id",
+  UserAuth,
+  authorize(AuthRoles.ADMIN, AuthRoles.VENDOR),
+  productByCollectionId
+);
+
+export default router;
