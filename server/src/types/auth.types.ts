027a86ab5f10b52fa215edd7b0e132364b6179eb
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-31 00:09:10 +0530

    feat(types): add auth type definition

---
+++ server/src/types/auth.types.ts
@@ -0,0 +1,22 @@
+import { Document, Types } from "mongoose";
+import AuthRoles from "../constants/authRoles";
+
+export type User = Document & {
+  name: string;
+  email: string;
+  password: string;
+  role: (typeof AuthRoles)[keyof typeof AuthRoles];
+  isVerified: boolean;
+  verifyOtp?: string;
+  verifyOtpExpireAt?: Date;
+  resetOtp?: string;
+  resetOtpExpireAt?: Date;
+  refreshToken?: string;
+  forgotPasswordToken?: string;
+  forgotPasswordExpire?: Date;
+
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
+  generateForgotPasswordToken(): string;
+  generateEmailVerification(): string;
+};
