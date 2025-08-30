cde2c3fd9c286f026fc72fc1b32dd13838aef519
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-31 00:13:57 +0530

    chore(types): export all type definitions from a single index file

---
+++ server/src/types/index.ts
@@ -0,0 +1,7 @@
+import { User } from "./auth.types";
+import { Product, OrderProduct } from "./product.types";
+import { Collection } from "./collection.types";
+import { Order } from "./order.types";
+import { Coupon } from "./coupon.types";
+
+export type { User, Product, OrderProduct, Collection, Order, Coupon };
