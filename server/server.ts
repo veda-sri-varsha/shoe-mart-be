4bf3a1263dd893d2d76f6c174e0683b168f388e3
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-09-04 14:30:18 +0530

    chore(server): setup server.ts with MongoDB connection and app listening

--- server/server.ts
+++ server/server.ts
@@ -1,25 +1,25 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import app from "./src/app";
-import config from "./src/config/index";
+  import mongoose from "mongoose";
+  import dotenv from "dotenv";
+  import app from "./src/app";
+  import config from "./src/config/index";
 
-dotenv.config();
+  dotenv.config();
 
-(async () => {
-  try {
-    await mongoose.connect(config.DATABASE_URL);
-    console.log("DATABASE CONNECTED SUCCESSFULLY");
+  (async () => {
+    try {
+      await mongoose.connect(config.DATABASE_URL);
+      console.log("DATABASE CONNECTED SUCCESSFULLY");
 
-    app.on("error", (error) => {
+      app.on("error", (error) => {
+        console.error("ERROR :", error);
+        throw error;
+      });
+
+      app.listen(config.PORT, () => {
+        console.log(`Server is Listening on http://localhost:${config.PORT}`);
+      });
+    } catch (error) {
       console.error("ERROR :", error);
       throw error;
-    });
-
-    app.listen(config.PORT, () => {
-      console.log(`Server is Listening on http://localhost:${config.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR :", error);
-    throw error;
-  }
-})();
+    }
+  })();
